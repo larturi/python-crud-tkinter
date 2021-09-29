e6a7668d2caae957a79b4a50ff6705f28a232c58
lea.arturi <lea.arturi@gmail.com>
2021-09-29 11:23:58 -0300

    Optimizacion

--- crud_python.py
+++ crud_python.py
@@ -12,6 +12,52 @@ DB_NAME = "Usuarios.sqlite"
 ############ FUNCIONES #############
 ####################################
 
+def makeMenu():
+
+    menu_barra = Menu(root)
+    root.config(menu=menu_barra)
+
+    menu_bd = Menu(menu_barra, tearoff=0)
+    menu_bd.add_command(label="Conectar", command=conectarDB)
+    menu_bd.add_command(label="Salir", command=salir)
+
+    menu_borrar = Menu(menu_barra, tearoff=0)
+    menu_borrar.add_command(label="Borrar campos", command=limpiarCampos)
+
+    menu_crud = Menu(menu_barra, tearoff=0)
+    menu_crud.add_command(label="Create", command=create)
+    menu_crud.add_command(label="Read")
+    menu_crud.add_command(label="Update")
+    menu_crud.add_command(label="Delete")
+
+    menu_help = Menu(menu_barra, tearoff=0)
+    menu_help.add_command(label="Licencia")
+    menu_help.add_command(label="About")
+
+    menu_barra.add_cascade(label="BBDD", menu=menu_bd)
+    menu_barra.add_cascade(label="Borrar", menu=menu_borrar)
+    menu_barra.add_cascade(label="CRUD", menu=menu_crud)
+    menu_barra.add_cascade(label="Ayuda", menu=menu_help)
+
+def labels():
+    label_id = Label(mi_frame, text="Id:")
+    label_id.grid(row=0, column=0, sticky="w", padx=10, pady=10)
+
+    label_nombre = Label(mi_frame, text="Nombre:")
+    label_nombre.grid(row=1, column=0, sticky="w", padx=10, pady=10)
+
+    label_apellido = Label(mi_frame, text="Apellido:")
+    label_apellido.grid(row=2, column=0, sticky="w", padx=10, pady=10)
+
+    label_password = Label(mi_frame, text="Password:")
+    label_password.grid(row=3, column=0, sticky="w", padx=10, pady=10)
+
+    label_direccion = Label(mi_frame, text="Direccion:")
+    label_direccion.grid(row=4, column=0, sticky="w", padx=10, pady=10)
+
+    label_comentarios = Label(mi_frame, text="Comentarios:")
+    label_comentarios.grid(row=5, column=0, sticky="w", padx=10, pady=10)
+
 def conectarDB():
     
     try:
@@ -71,59 +117,12 @@ root.title("Python CRUD")
 root.maxsize(340, 380) 
 root.minsize(340, 380) 
 
-####################################
-############### MENU ###############
-####################################
-
-menu_barra = Menu(root)
-root.config(menu=menu_barra)
-
-menu_bd = Menu(menu_barra, tearoff=0)
-menu_bd.add_command(label="Conectar", command=conectarDB)
-menu_bd.add_command(label="Salir", command=salir)
-
-menu_borrar = Menu(menu_barra, tearoff=0)
-menu_borrar.add_command(label="Borrar campos", command=limpiarCampos)
-
-menu_crud = Menu(menu_barra, tearoff=0)
-menu_crud.add_command(label="Create", command=create)
-menu_crud.add_command(label="Read")
-menu_crud.add_command(label="Update")
-menu_crud.add_command(label="Delete")
-
-menu_help = Menu(menu_barra, tearoff=0)
-menu_help.add_command(label="Licencia")
-menu_help.add_command(label="About")
-
-menu_barra.add_cascade(label="BBDD", menu=menu_bd)
-menu_barra.add_cascade(label="Borrar", menu=menu_borrar)
-menu_barra.add_cascade(label="CRUD", menu=menu_crud)
-menu_barra.add_cascade(label="Ayuda", menu=menu_help)
-
 mi_frame = Frame(root)
 mi_frame.pack()
 
-####################################
-############## LABELS ##############
-####################################
+makeMenu()
+labels()
 
-label_id = Label(mi_frame, text="Id:")
-label_id.grid(row=0, column=0, sticky="w", padx=10, pady=10)
-
-label_nombre = Label(mi_frame, text="Nombre:")
-label_nombre.grid(row=1, column=0, sticky="w", padx=10, pady=10)
-
-label_apellido = Label(mi_frame, text="Apellido:")
-label_apellido.grid(row=2, column=0, sticky="w", padx=10, pady=10)
-
-label_password = Label(mi_frame, text="Password:")
-label_password.grid(row=3, column=0, sticky="w", padx=10, pady=10)
-
-label_direccion = Label(mi_frame, text="Direccion:")
-label_direccion.grid(row=4, column=0, sticky="w", padx=10, pady=10)
-
-label_comentarios = Label(mi_frame, text="Comentarios:")
-label_comentarios.grid(row=5, column=0, sticky="w", padx=10, pady=10)
 
 ####################################
 ############## CAMPOS ##############
